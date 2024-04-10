c03e2a86ac1072abedb9ba49392b3a4af6b7555b
Jesus-Garcia-Montalvo <jesus.antonio.garcia9871@gmail.com>
2024-04-09 20:06:40 -0500

    card de mascotas

--- client/src/components/PetCard/PetCard.jsx
+++ client/src/components/PetCard/PetCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./PetCard.scss";
+import petImage from "../../assets/images/pet-image.jpg";
 
 function PetCard() {
   return (
     <>
       <div className="card">
-        <div className="containerImages">
-          <h1>images</h1>
-        </div>
+        <div className="containerImages"></div>
         <div className="containerTitleDescipcion">
-          <h1 className="title">Title</h1>
+          <h1 className="title">Canela</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
             deserunt dola
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,8 @@
-import LandingPage from "@/components/landing-page/LandingPage";
 import React from "react";
 
 function Home() {
   return (
     <>
-      <LandingPage />
       <div className="text-xl text-blue-500">Home</div>
     </>
   );
