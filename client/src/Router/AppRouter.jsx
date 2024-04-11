29ef02a3f86861c8430d9e8ce2dd8c095fdb7d31
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-10 20:35:25 -0600

    add import alias

--- client/src/Router/AppRouter.jsx
+++ client/src/Router/AppRouter.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
-import Register from "../pages/register/Register";
-import Login from "../pages/login/Login";
-import Navbar from '../components/Navbar';
+import Home from "@pages/home/Home";
+import Register from "@pages/register/Register";
+import Navbar from '@components/Navbar';
+import Login from '@pages/login/Login';
 
 export default function AppRouter() {
   return (
