77fb5c215574e30c2d09e142292ffd0ab87d7eaa
emersonhv <emersonhv@gmail.com>
2024-04-16 17:50:06 -0500

    Agregar nuevo atributo a clase PET, name

--- pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Pet/Model/Pet.java
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Pet/Model/Pet.java
@@ -19,6 +19,8 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "ID")
     private String id;
+    @Column(name = "NAME")
+    private String name;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
     private User user;
--- pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/User/Controllers/v1/UserController.java
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/User/Controllers/v1/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    @GetMapping("/")
+    @GetMapping("/user")
     public ResponseEntity<String> getAllUser(){
         return ResponseEntity.ok("Todo Nice");
     }
