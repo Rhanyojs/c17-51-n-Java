4a537f571b168d214c7f0d09d82d4c2b7487e5f2
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-17 19:29:07 -0600

    adding auth validation

--- client/src/pages/nosotros/Nosotros.jsx
+++ client/src/pages/nosotros/Nosotros.jsx
@@ -13,28 +13,28 @@ const Nosotros = () => {
           Además, los dueños de mascotas en todo Estados Unidos eligen nuestro salón para:</p>
          <div className="container-beneficios">
             <div>
-            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fd0061" fill="none" strokeLinecap="round" strokeLinejoin="round">
            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
            <path d="M15 3a3 3 0 0 1 3 3a3 3 0 1 1 -2.12 5.122l-4.758 4.758a3 3 0 1 1 -5.117 2.297l0 -.177l-.176 0a3 3 0 1 1 2.298 -5.115l4.758 -4.758a3 3 0 0 1 2.12 -5.122z" />
            </svg>
                 Ambiente sano y seguro
             </div>
             <div>
-            <svg className="icono-hueso"  xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fd0061" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M15 3a3 3 0 0 1 3 3a3 3 0 1 1 -2.12 5.122l-4.758 4.758a3 3 0 1 1 -5.117 2.297l0 -.177l-.176 0a3 3 0 1 1 2.298 -5.115l4.758 -4.758a3 3 0 0 1 2.12 -5.122z" />
             </svg>
                 Equipos de alta calidad y totalmente estériles
             </div>
             <div className="texto-items">
-            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fd0061" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M15 3a3 3 0 0 1 3 3a3 3 0 1 1 -2.12 5.122l-4.758 4.758a3 3 0 1 1 -5.117 2.297l0 -.177l-.176 0a3 3 0 1 1 2.298 -5.115l4.758 -4.758a3 3 0 0 1 2.12 -5.122z" />
             </svg>
                 Diferentes tipos de aseo para tus mascotas
             </div>
             <div >
-            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="icono-hueso" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bone" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fd0061" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M15 3a3 3 0 0 1 3 3a3 3 0 1 1 -2.12 5.122l-4.758 4.758a3 3 0 1 1 -5.117 2.297l0 -.177l-.176 0a3 3 0 1 1 2.298 -5.115l4.758 -4.758a3 3 0 0 1 2.12 -5.122z" />
             </svg>
