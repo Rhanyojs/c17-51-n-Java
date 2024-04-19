c8e26cf05ec417c2d93a732de1d00f1f7e5be807
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-04-18 22:21:36 -0500

    Update RequestServices.java

--- pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Request/Services/RequestServices.java
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Request/Services/RequestServices.java
@@ -1,4 +1,9 @@
 package tech.nocountry.c1751njava.petadoption.Request.Services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RequestServices {
 }
