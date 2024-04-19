80543e3ee7e0a7b2aebf67df092fdd6da70ebe8f
Jesus-Garcia-Montalvo <jesus.antonio.garcia9871@gmail.com>
2024-04-19 13:35:52 -0500

    validacion de formularios

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Login.scss";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { FaUser } from "react-icons/fa";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    await Swal.fire({
+      icon: "success",
+      title: "¡Iniciar sesión exitoso!",
+      text: "Has iniciado sesión  correctamente.",
+    });
+    reset();
+  };
   return (
     <div className="login-container">
       <h2 className="register-title">Iniciar sesión</h2>
-      <form className="login-form">
+      <div className="container-login-icon">
+        <FaUser className="login-icon" />
+      </div>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="login-input-container">
           <label htmlFor="email" className="login-label">
             Correo electrónico
@@ -13,9 +35,20 @@ const Login = () => {
           <input
             type="email"
             className="login-input"
-            id="email"
             placeholder="Ingresa tu correo electrónico"
+            {...register("correo", {
+              required: "El correo es requerido",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+
+                message: "El correo no es válido",
+              },
+            })}
           />
+          {errors.correo &&
+            (touchedFields.correo || errors.correo.type === "required") && (
+              <span className="login-span">{errors.correo.message}</span>
+            )}
         </div>
         <div className="login-input-container">
           <label htmlFor="password" className="login-label">
@@ -24,9 +57,24 @@ const Login = () => {
           <input
             type="password"
             className="login-input"
-            id="password"
             placeholder="Ingresa tu contraseña"
+            {...register("contraseña", {
+              required: "La contraseña es requerida",
+              minLength: {
+                value: 6,
+                message: "La contraseña debe ser mayor a 6 caracteres",
+              },
+              maxLength: {
+                value: 16,
+                message: "La contraseña debe ser menor a 16 caracteres",
+              },
+            })}
           />
+          {errors.contraseña &&
+            (touchedFields.contraseña ||
+              errors.contraseña.type === "required") && (
+              <span className="login-span">{errors.contraseña.message}</span>
+            )}
         </div>
         <div className="login-remember">
           <div className="login-checkbox-container">
@@ -34,7 +82,6 @@ const Login = () => {
             <p>recordar contraseña</p>
           </div>
         </div>
-
         <button type="submit" className="login-button">
           Iniciar sesión
         </button>
