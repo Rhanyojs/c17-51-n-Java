5e70a5c2fcf9195f589dab32b0b68b68148f19cd
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-04-21 13:04:40 -0500

    Actualizacion Request

---
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Request/Controller/v1/RequestController.java
@@ -0,0 +1,10 @@
+package tech.nocountry.c1751njava.petadoption.Request.Controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/request")
+public class RequestController {
+
+}
--- pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Request/Services/RequestServices.java
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Request/Services/RequestServices.java
@@ -1,6 +1,45 @@
 package tech.nocountry.c1751njava.petadoption.Request.Services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.nocountry.c1751njava.petadoption.Request.Repository.RequestRepository;
+import tech.nocountry.c1751njava.petadoption.Request.Request;
+import tech.nocountry.c1751njava.petadoption.Request.State;
 
-public interface RequestServices {
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RequestServices {
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    public List<Request> getAllRequests() {
+        return requestRepository.findAll();
+    }
+
+    public Request getRequestById(String id) {
+        return requestRepository.findById(id).orElseThrow(() -> new RuntimeException("Request not found"));
+    }
+
+    public Request createRequest(Request request) {
+        return requestRepository.save(request);
+    }
+
+    public Request updateRequest(Request request) {
+        return requestRepository.save(request);
+    }
+
+    public void deleteRequest(String id) {
+        requestRepository.deleteById(id);
+    }
+
+    public Request changeRequestState(String requestId, State newState) {
+        Request request = getRequestById(requestId);
+        request.setState(newState);
+        return requestRepository.save(request);
+    }
 
 }
