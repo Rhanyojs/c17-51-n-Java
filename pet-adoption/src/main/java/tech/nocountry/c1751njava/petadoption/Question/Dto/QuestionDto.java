5f041710490217bcc6ed83b350e49d725ca76d78
MiguelAChavez <129125500+MiguelAChavez@users.noreply.github.com>
2024-04-21 10:42:26 -0300

    Update QuestionDto.java

--- pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Question/Dto/QuestionDto.java
+++ pet-adoption/src/main/java/tech/nocountry/c1751njava/petadoption/Question/Dto/QuestionDto.java
@@ -9,11 +9,13 @@ import lombok.Data;
 public class QuestionDto {
 
     private String id;
+
     @NotBlank(message = "Question cannot be blank")
     private String question;
+
     @NotBlank(message = "Answer cannot be blank")
     private String answer;
-    @NotBlank(message = "FormId cannot be blank")
+
     private String formId;
 
 }
